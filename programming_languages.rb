c7437915575bacbd23c1070127494b6f28359a02
Kesean Woodhouse <kesean.woodhouse@gmail.com>
2017-07-05 23:37:08 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, language_hash|
+    language_hash.each do |language, data|
+      new_hash[language] ||= data
+      new_hash[language][:style] ||= []
+      new_hash[language][:style] << type
+    end
+  end
+  return new_hash
 end
